531ff1fe649b44d7c2fa9504091591be6279f578
septiian07 <72737066+septiian07@users.noreply.github.com>
2020-10-16 14:12:14 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+ 	<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+	<link rel="icon" type="image/png" href="../img/icon/stm.png"/>
+	<title>Sistem Informasi Keuangan</title>
+</head>
+<body style="background-image: url(../img/bg/bg3.jpg); background-attachment: fixed; background-position: right bottom;">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<form class="box">
+					<h3>Login</h3>
+					<div class="form-group">
+						<br>
+						<label>NIP</label>
+						<input type="number" class="form-control" placeholder="NIP">
+					</div>
+					<div class="form-group">
+						<label>Password</label>
+						<input type="password" class="form-control" placeholder="Password">
+					</div>
+					<div>
+						<a href="index-kepsek.php"><input type="submit" class="btn btn-default center sign" value="Sign In"></a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
